d1adab7ce28516210ccaa9dcb5441f4a94edc559
oemc <oscareduardomontalvo@gmail.com>
2023-04-22 18:17:27 -0500

    se agrega puerto generico para despliegue

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(app.get('port'));
+console.log('Server on port 3000');
