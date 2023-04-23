cc11201829ee566eee297467b8d80f6795af3382
oemc <oscareduardomontalvo@gmail.com>
2023-04-23 10:41:36 -0500

    se restablece configuracion

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use(cors());
-
 app.use("/", route);
 
 app.use(express.static(path.join(__dirname, 'public')));
