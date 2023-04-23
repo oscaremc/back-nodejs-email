4be77512758838133d46474eb18358ee3bcd6413
oemc <oscareduardomontalvo@gmail.com>
2023-04-22 23:18:33 -0500

    se elimina configuracion de cors

--- src/index.js
+++ src/index.js
@@ -9,23 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:4200/home', 'https://oscaremc.com/home'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
+app.use();
 
 app.use("/", route);
 
