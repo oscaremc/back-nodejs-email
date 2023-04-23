4242576b49133fb503f2a2330e5318dd74ea31e4
oemc <oscareduardomontalvo@gmail.com>
2023-04-22 19:07:05 -0500

    se agrega configuracion de cors

--- src/index.js
+++ src/index.js
@@ -5,9 +5,23 @@ const path = require('path');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+// Configurar sistema de logs
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
 app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 4000);
 console.log('Servidor conectado', app.get('port'));
+
