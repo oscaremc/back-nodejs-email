861d025370484dbbfd815191b26086189c2d41c1
Oscar Eduardo Montalvo Camacho <franknicolas.herran.contractor.cidentity@bbva.com>
2023-08-01 21:43:28 -0500

    se ajusta puerto

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const route = require('./routes/index');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(cors());
 
